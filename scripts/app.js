a84a9baa8a335dee0c0c31feffd97bcf242b48cf
Ash Atighetchi <ashgetchi@gmail.com>
2020-07-18 16:48:35 +0100

    template for planning

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,28 @@
 function init() {
 
+  //? variables
+
+  let width = 10
+  let height = 10
+  let sizeOfGrid = width * height
+  const cells = []
+
+
+//? Dom Elements
+const thegrid = document.querySelector('#thegrid')
+let 
 
   console.log('poo poo')
+
+  //? functions 
+function createGrid() {
+  for (i = 0; i < sizeOfGrid; i++){
+  const cell = document.createElement('div')
+  thegrid.appendChild(cell)
+  }
+}
+createGrid()
+  //? Events
 
 
 
